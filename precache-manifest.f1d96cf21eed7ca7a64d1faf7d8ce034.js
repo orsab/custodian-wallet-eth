6a04756ff6a1c55ad533c6fd6f815eedebe9ca89
orsab1@gmail.com <orsab1@gmail.com>
2022-05-16 08:34:14 +0300

    Updates

---
+++ precache-manifest.f1d96cf21eed7ca7a64d1faf7d8ce034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c71723cfd5ab3e0acd29b5aa77aff63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eebeca44a0890ad768f",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "2eebeca44a0890ad768f",
+    "url": "/static/js/2.e18bd9ce.chunk.js"
+  },
+  {
+    "revision": "d57dae176fb87a393a80e02d0e438720",
+    "url": "/static/js/2.e18bd9ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eeed0dda900b8a74a4c",
+    "url": "/static/js/main.9f40a92f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
